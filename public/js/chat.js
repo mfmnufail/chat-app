850550a69151988ace61ae297d929d533db71c20
Mohamed Nufail <eg173254@engug.ruh.ac.lk>
2022-05-24 18:21:25 +0530

    Added element attributes

--- public/js/chat.js
+++ public/js/chat.js
@@ -3,23 +3,38 @@ const socket = io();
 socket.on("message", (message) => {
   console.log(message);
 });
-
 window.onload = function () {
-  document.querySelector("#message-form").addEventListener("submit", (e) => {
+const $messageForm = document.querySelector("#message-form");
+const $messageInput = $messageForm.querySelector("input")
+const $messageButton = $messageForm.querySelector("button")
+const $sendLocation = document.querySelector("#send-location");
+
+
+  $messageForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    //   const message = e.target.elements.message.value;
-    const message = e.target.elements.message.value;
-    socket.emit("chat", message, (error)=>{
+      // const message = e.target.elements.message.value;
+      const message = e.target.elements.message.value;
+
+      $messageButton.setAttribute('disabled', "disabled")
       
+      socket.emit("chat", message, (error)=>{
+        
+        $messageInput.value = ''
+        $messageInput.focus()
+        
       if(error){
         return console.log(error)
       }
       console.log(`>> ${message}`);
       console.log("Message delivered successfully!")
+
+      $messageButton.removeAttribute("disabled")
     });
   });
 
-  document.querySelector("#send-location").addEventListener("click", () => {
+  $sendLocation.addEventListener("click", () => {
+
+    $sendLocation.setAttribute('disabled','disabled')
     if (!navigator.geolocation) {
       return alert("Geolocatoin is not supported by your browser");
     }
@@ -32,6 +47,7 @@ window.onload = function () {
         long: location.coords.longitude,
       }, ()=>{
         console.log("Location shared successfully!")
+        $sendLocation.removeAttribute('disabled')
       });
     });
   });
